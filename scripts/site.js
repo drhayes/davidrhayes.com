ac87e76ac29e2ebccac0490d2f616823a41f665d
David Hayes <drhayes@gmail.com>
2010-04-05 22:52:51 -0700

    Broke it. Fixing.

--- scripts/site.js
+++ scripts/site.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 
-	var push_letters = function(text, target) {
+	var push_letters = function(text, target, callback) {
 		if (!text) {
 			$(target).parents('.termouter').removeClass('withcursor');
+			if (callback) {
+				callback();
+			}
 			return;
 		}
 		var current = $(target).text();
@@ -13,21 +16,25 @@ $(document).ready(function() {
 		}, 50);
 	};
 
-	var terminal_type = function(text, target) {
-		var target = target || 'body';
-		var elem = $('<span class="termouter withcursor"><p class="terminner"></p></span>');
-		$(elem).appendTo(target);
-		// show the cursor for a bit
-		var inner = $(elem).children('.terminner');
-		// inner.html('&nbsp;');
-		setTimeout(function() {
-			push_letters(text, inner);
-		}, 1000);
+	var terminal_type = function(lines, target) {
+		if (!$.isArray(lines)) {
+			lines = [lines];
+		}
+		target = target || 'body';
+		for (var i=lines.length; i < len; i++) {
+			var elem = $('<span class="termouter withcursor"><p class="terminner"></p></span><br />');
+			$(elem).appendTo(target);
+			var inner = $(elem).children('.terminner');
+			setTimeout(function() {
+				push_letters(lines[i], inner);
+			}, 1000);
+		};
 	};
 	
 	// don't start everything for a bit...
 	setTimeout(function() {
-		terminal_type('Subject: David R. Hayes');
-		terminal_type('Username: drhayes');
+		terminal_type(
+			['Subject:David R. Hayes',
+			'Username: drhayes'], '#content');
 	}, 1500);
 });
