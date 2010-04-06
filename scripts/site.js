e28669640ce716063dd9cbc1d5d47ea4da5a69c8
David Hayes <drhayes@gmail.com>
2010-04-05 21:41:51 -0700

    The blank slate

--- scripts/site.js
+++ scripts/site.js
@@ -1,8 +1,2 @@
 $(document).ready(function() {
-    // hide the extended me
-    $('#meonline ul').hide();
-    // turn the extended me into an expanding list
-    $('#meonline > a').click(function() {
-        $('#meonline ul').toggle('slow');
-    })
 });
