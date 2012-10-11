b814d23f92d4b9833bc558bd8264b3bba95747ea
David Hayes <drhayes@gmail.com>
2012-10-10 23:06:23 -0700

    Trying to get an automated build file going.
    Think I've got it.

--- grunt.js
+++ grunt.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+
   grunt.initConfig({
     test: {
       all: ['test/**/*.js']
@@ -7,10 +9,42 @@ module.exports = function(grunt) {
 
     lint: {
       all: ['grunt.js', 'js/app/**/*.js']
+    },
+
+    requirejs: {
+
+      compile: {
+        options: {
+          baseUrl: 'js/app',
+
+          paths: {
+            'knockout': '../lib/ko/knockout-2.1.0',
+            'underscore': '../lib/underscore/underscore-min',
+            'handlebars': '../lib/handlebars/handlebars-1.0.rc.1',
+            'hashchange': '../lib/hashchange/jquery.ba-hashchange.min',
+            'mousewheel': '../lib/mousewheel/jquery.mousewheel',
+            'text': '../lib/require/plugins/text'
+          },
+
+          shim: {
+            'underscore': {
+              'exports': '_'
+            },
+            'handlebars': {
+              'exports': 'Handlebars'
+            }
+          },
+
+          name: 'main',
+          out: 'js/dist/main.js'
+        }
+      }
     }
   });
 
   // Default task.
   grunt.registerTask('default', 'lint test');
+
+  grunt.registerTask('build', 'requirejs');
 
 };
