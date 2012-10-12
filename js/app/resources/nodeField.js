3a3deb8d8935e4729a01cccdd96bbbe559fba7a0
David Hayes <drhayes@gmail.com>
2012-10-12 00:05:55 -0700

    Show the current stats in the upper-right.

--- js/app/resources/nodeField.js
+++ js/app/resources/nodeField.js
@@ -1,13 +1,25 @@
 /*global define */
 define([
   'knockout',
+  'underscore',
   'resources/viewport'
-], function(ko, Viewport) {
+], function(ko, _, Viewport) {
+
+  var makeNodeReducer = function(propertyName, nodesAccessor) {
+    return function() {
+      var iterator = function(memo, node) {
+        return memo + (node[propertyName] && node[propertyName]()) || 0;
+      };
+      return _.reduce(nodesAccessor(), iterator, 0);
+    };
+  };
+
   // Container for nodes.
   return function() {
     var self = this;
     self.nodes = ko.observableArray();
-
+    self.generatorPoints = ko.computed(makeNodeReducer('points', self.nodes));
+    self.maxGeneratorPoints = ko.computed(makeNodeReducer('maxPoints', self.nodes));
     self.viewport = new Viewport();
   };
 });
