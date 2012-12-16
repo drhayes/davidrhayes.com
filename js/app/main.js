5e0b049e7395b5865402f57eeda5fee3fd4106f9
David Hayes <drhayes@gmail.com>
2012-12-15 22:39:41 -0800

    Ripping out the world.

--- js/app/main.js
+++
@@ -1,9 +0,0 @@
-/*global require */
-require(['frontpage/frontpage', 'system/urlHash'], function(frontpage, urlHash) {
-  "use strict";
-
-  frontpage();
-
-  // Get things going.
-  urlHash.onHashchange();
-});
