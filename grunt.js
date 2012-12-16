1a20479536eafe9c2859620dfd8baa15f7eb3ae4
David Hayes <drhayes@gmail.com>
2012-12-15 22:41:05 -0800

    Rebuilding stuff.

--- grunt.js
+++ grunt.js
@@ -1,9 +1,5 @@
 module.exports = function(grunt) {
 
-  // Load our external task things.
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-less');
-
   grunt.initConfig({
     test: {
       all: ['test/**/*.js']
@@ -13,36 +9,10 @@ module.exports = function(grunt) {
       all: ['grunt.js', 'js/app/**/*.js']
     },
 
-    requirejs: {
-      compile: {
-        options: {
-          baseUrl: 'js/app',
-
-          paths: {
-            'knockout': '../lib/ko/knockout-2.1.0',
-            'underscore': '../lib/underscore/underscore-min',
-            'hashchange': '../lib/hashchange/jquery.ba-hashchange.min',
-            'mousewheel': '../lib/mousewheel/jquery.mousewheel',
-            'text': '../lib/require/plugins/text',
-            'signals': '../lib/signals/signals.min'
-          },
-
-          shim: {
-            'underscore': {
-              'exports': '_'
-            }
-          },
-
-          name: 'main',
-          out: 'js/dist/main.js'
-        }
-      }
-    },
-
     less: {
       dev: {
         options: {
-          paths: ['less/app', 'less/lib/bootstrap', 'less/lib/font-awesome'],
+          paths: ['less/app'],
           compress: true
         },
         files: {
@@ -60,6 +30,6 @@ module.exports = function(grunt) {
   // Default task.
   grunt.registerTask('default', 'lint test');
 
-  grunt.registerTask('build', 'less:dev requirejs');
+  grunt.registerTask('build', 'less:dev');
 
 };
